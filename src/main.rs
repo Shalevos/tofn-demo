127d902a5770d592674aacd4876f50b4dab9e9d8
shalevos <shalevos@shalevos.io>
2022-07-06 00:24:32 +0300

    added multi thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod common;
 mod execute;
 mod single_thread;
+mod party;
+mod multi_thread;
 
 use single_thread::{basic_correctness as single_thread_basic_correctness};
+use multi_thread::{basic_correctness as multi_thread_basic_correctness};
 
 fn main() {
     single_thread_basic_correctness();
+    multi_thread_basic_correctness();
 }
